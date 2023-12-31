80ff67e7fd4aca40f662e5f9301e9eb88286b998
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 14:11:58 +0100

    3 descb

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -4,6 +4,3 @@ Type-annotated function to_str that takes a float n & returns the string repr
 """
 
 
-def to_str(n: float) -> str:
-    """Return string representation of float"""
-    return str(n)
