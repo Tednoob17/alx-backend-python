d0c8352a14d0cabcd2961dabe2ab9dfebde6ad2e
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 15:33:28 +0100

    main async

--- 0x02-python_async_comprehension/main.py
+++ 0x02-python_async_comprehension/main.py
@@ -4,7 +4,20 @@
 
 # async_generator = __import__('0-async_generator').async_generator
 
+# async def print_yielded_values():
+#     result = []
+#     async for i in async_generator():
+#         result.append(i)
+#     print(result)
 
+# asyncio.run(print_yielded_values())
+
+# import asyncio
+
+# async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+# asyncio.run(main())
 
 import asyncio
 
