dccc048ddc747ccb59aad914e12fe46b111d95c7
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 15:27:31 +0100

    2 time and perf

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,3 +4,14 @@ Measure the runtime
 """
 import time
 import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measure the runtime
+    """
+    start = time.perf_counter()
+    tasks = [async_comprehension() for i in range(4)]
+
+    return (end - start)
