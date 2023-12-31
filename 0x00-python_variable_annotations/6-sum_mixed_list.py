eee104c620651bdf5455b211ea1e7cdf103ef87a
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 14:17:07 +0100

    6 sum mixed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,3 @@ Type-annotated sum_mixed_list function
 """
 from typing import List, Union
 
-
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
-    """Return sum of mxd_lst"""
-    return float(sum(mxd_lst))
