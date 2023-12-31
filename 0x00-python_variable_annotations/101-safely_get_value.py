551b5a85411c838f5af63f29212b8e1a7c99336f
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 14:09:37 +0100

    typevar optiona

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,12 +4,3 @@ Safety get value
 """
 from typing import Any, Mapping, TypeVar, Optional, Union, Any
 
-T = TypeVar('T')
-
-
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Optional[T] = None) -> Union[Any, T]:
-    if key in dct:
-        return dct[key]
-    else:
-        return default
