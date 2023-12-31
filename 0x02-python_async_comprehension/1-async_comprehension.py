e4afde9b77a4597127450b46ec3fca3da8126138
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 15:26:09 +0100

    1 end

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-
+    """
+    Async Comprehensions
+    """
     return [i async for i in async_generator()]
