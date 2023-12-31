c0461ac8f962cfbf9e23a4304f6207ed2d883f6b
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 14:41:33 +0100

    place in main

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 
+# add = __import__('0-add').add
+
+# print(add(1.11, 2.22) == 1.11 + 2.22)
+# print(add.__annotations__)
+
+# concat = __import__('1-concat').concat
+
+# str1 = "egg"
+# str2 = "shell"
+
+# print(concat(str1, str2) == "{}{}".format(str1, str2))
+# print(concat.__annotations__)
+
+# import math
+
+# floor = __import__('2-floor').floor
+
+# ans = floor(3.14)
+
+# print(ans == math.floor(3.14))
+# print(floor.__annotations__)
+# print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
+
+# to_str = __import__('3-to_str').to_str
+
+# pi_str = to_str(3.14)
+# print(pi_str == str(3.14))
 # print(to_str.__annotations__)
 # print("to_str(3.14) returns {} which is a {}".format(pi_str, type(pi_str)))
 
