7d7eb6a411fede81668e2ef9744edde2c6bb8a7b
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 14:19:55 +0100

    ml float

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,9 +2,3 @@
 """
 Multiply a float
 """
-from typing import Callable
-
-
-def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Return function that multiplies a float by multiplier"""
-    return lambda x: x * multiplier
