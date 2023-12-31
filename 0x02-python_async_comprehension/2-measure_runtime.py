840e452e71eb686ed0d3fa661949bdac6dd0bd17
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 15:27:15 +0100

    2 measure rntm

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,15 +4,3 @@ Measure the runtime
 """
 import time
 import asyncio
-async_comprehension = __import__('1-async_comprehension').async_comprehension
-
-
-async def measure_runtime() -> float:
-    """
-    Measure the runtime
-    """
-    start = time.perf_counter()
-    tasks = [async_comprehension() for i in range(4)]
-    await asyncio.gather(*tasks)
-    end = time.perf_counter()
-    return (end - start)
